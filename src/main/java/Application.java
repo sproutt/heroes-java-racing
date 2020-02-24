6a811a3bf9cb71313af2aac7a2b613d25d9c7ecf
배종진 <qowhdwlsdk@gmail.com>
2020-02-25 08:35:33 +0900

    Creates New Racing Game Project Structure

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,10 +1,5 @@
-import controller.RacingGameController;
-import exception.OutOfCarNameLengthException;
-
 public class Application {
-    public static void main(String[] args) throws OutOfCarNameLengthException {
-        RacingGameController racingGameController = new RacingGameController();
-        racingGameController.set();
-        racingGameController.start();
+    public static void main(String[] args) {
+
     }
 }
