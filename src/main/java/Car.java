31dc5c5d9a8101fa97d8aaaeb55bca162bc18ab6
saint6839 <saint6839@gmail.com>
2022-02-10 22:41:32 +0900

    refactor: 차 전진 기준 상수화

--- src/main/java/Car.java
+++ src/main/java/Car.java
@@ -1,9 +1,11 @@
 public class Car {
+    private static final int CAR_PROCEED_CRITERIA = 4;
+
     private int number;
     private int position;
 
     public void move(int randomValue) {
-        if (randomValue > 4) {
+        if (randomValue > CAR_PROCEED_CRITERIA) {
             position++;
         }
     }
