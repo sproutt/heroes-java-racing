687017a136711621aea056cef5230565e19a6219
saint6839 <saint6839@gmail.com>
2022-02-10 23:27:38 +0900

    refactor: InputView 클래스 메서드 static으로 변경
    인스턴스 변수가 존재하지 않고 매개변수에만 의존하기 때문에 static으로 변경
    모든 메소드가 static이고 객체를 만들어 쓰진 않는 클래스를 유틸리티 클래스라고 칭함

--- src/main/java/InputView.java
+++ src/main/java/InputView.java
@@ -1,17 +1,17 @@
 import java.util.Scanner;
 
 public class InputView {
-    private final String CAR_NUMBER_INPUT_MESSAGE = "자동차 대수는 몇 대 인가요?";
-    private final String GAME_TRIAL_INPUT_MESSAGE = "시도할 횟수는 몇 회 인가요?";
+    private static final String CAR_NUMBER_INPUT_MESSAGE = "자동차 대수는 몇 대 인가요?";
+    private static final String GAME_TRIAL_INPUT_MESSAGE = "시도할 횟수는 몇 회 인가요?";
 
     private static Scanner scanner = new Scanner(System.in);
 
-    public int inputCarCount() {
+    public static int inputCarCount() {
         System.out.println(CAR_NUMBER_INPUT_MESSAGE);
         return scanner.nextInt();
     }
 
-    public int inputTrialCount() {
+    public static int inputTrialCount() {
         System.out.println(GAME_TRIAL_INPUT_MESSAGE);
         return scanner.nextInt();
     }
--- src/main/java/Main.java
+++
@@ -1,9 +0,0 @@
-import java.util.ArrayList;
-import java.util.Random;
-import java.util.Scanner;
-
-public class Main {
-    public static void main(String[] args) {
-
-    }
-}
