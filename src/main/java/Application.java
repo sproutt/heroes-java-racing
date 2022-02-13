eaf597a99da4d2ca3c742eb833b4df8507c672ed
sunghyuki <sunghyuk1609@gmail.com>
2022-02-13 20:31:34 +0900

    feat : Race 컨트롤러 구현 - 출전 자동차 등록 메서드 구현 - 한 라운드 진행 메서드 구현 - 시도 횟수만큼 라운드 진행 메서드 구현

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -2,6 +2,7 @@ import controller.Game;
 
 public class Application {
     public static void main(String[] args) {
-        Game.start();
+        Game game = new Game();
+        game.play();
     }
 }
