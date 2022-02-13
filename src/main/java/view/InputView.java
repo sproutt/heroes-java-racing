af7c8132856b4189f01abdb284c07d8000cb5637
sunghyuki <sunghyuk1609@gmail.com>
2022-02-14 00:49:37 +0900

    refactor : InputView 클래스 수정 - extract class for exception test - nextInt() -> nextLine()으로 수정(문자 예외 처리를 위해)

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,23 +1,45 @@
 package view;
 
+import exception.InputViewException;
+import view.validation.InputValidator;
+
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class InputView {
     private static final String INPUT_CARS_NUMBER = "자동차 대수는 몇 대 인가요?";
     private static final String INPUT_TRY_COUNT = "시도할 횟수는 몇 회 인가요?";
     private Scanner scanner;
+    private InputValidator inputValidator;
 
     public InputView(Scanner scanner) {
         this.scanner = scanner;
+        inputValidator = new InputValidator();
     }
 
     public int inputCarsNumber() {
         System.out.println(INPUT_CARS_NUMBER);
-        return scanner.nextInt();
+        String carsNumber = scanner.nextLine();
+        try {
+            inputValidator.validateNumber(carsNumber);
+            inputValidator.validatePositiveAndNotZeroNumber(Integer.parseInt(carsNumber));
+        } catch (InputViewException | InputMismatchException inputViewException) {
+            System.out.println(inputViewException.getMessage());
+            return inputCarsNumber();
+        }
+        return Integer.parseInt(carsNumber);
     }
 
     public int inputTryCount() {
         System.out.println(INPUT_TRY_COUNT);
-        return scanner.nextInt();
+        String tryCount = scanner.nextLine();
+        try {
+            inputValidator.validateNumber(tryCount);
+            inputValidator.validatePositiveAndNotZeroNumber(Integer.parseInt(tryCount));
+        } catch (InputViewException | InputMismatchException inputViewException) {
+            System.out.println(inputViewException.getMessage());
+            return inputTryCount();
+        }
+        return Integer.parseInt(tryCount);
     }
 }
