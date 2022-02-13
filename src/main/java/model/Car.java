badf007b187c79e71134a284e1525fc64be6a78d
sunghyuki <sunghyuk1609@gmail.com>
2022-02-13 20:15:29 +0900

    feat : Car 클래스 작성 - 자동차 전진 기능 구현

--- src/main/java/model/Car.java
+++ src/main/java/model/Car.java
@@ -1,4 +1,17 @@
 package model;
 
 public class Car {
+    private int position;
+
+    public Car() {
+        position = 0;
+    }
+
+    public void move() {
+        position++;
+    }
+
+    public int getPosition() {
+        return position;
+    }
 }
