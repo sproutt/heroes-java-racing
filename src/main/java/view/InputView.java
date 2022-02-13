0f497ff448a53d98d509a490e770f7565c9beef1
sunghyuki <sunghyuk1609@gmail.com>
2022-02-13 20:23:46 +0900

    feat : InputView 구현 - 자동차 대수 입력받기 - 게임 시도 횟수 입력받기

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,4 +1,23 @@
 package view;
 
+import java.util.Scanner;
+
 public class InputView {
+    private static final String INPUT_CARS_NUMBER = "자동차 대수는 몇 대 인가요?";
+    private static final String INPUT_TRY_COUNT = "시도할 횟수는 몇 회 인가요?";
+    private Scanner scanner;
+
+    public InputView(Scanner scanner) {
+        this.scanner = scanner;
+    }
+
+    public int inputCarsNumber() {
+        System.out.println(INPUT_CARS_NUMBER);
+        return scanner.nextInt();
+    }
+
+    public int inputTryCount() {
+        System.out.println(INPUT_TRY_COUNT);
+        return scanner.nextInt();
+    }
 }
