422f52ed9c9ab1caa4f60d5f6a57e50f969ea616
user <gusdn85554@naver.com>
2022-03-01 23:10:23 +0900

    feat : RacingApplication 구현
    게임을 시작하기 위해 객체 생성
    1. 자동차 개수, 시도 횟수 입력
    2. 자동차 등록
    3. 각 자동차 움직이기
    4. 모든 자동차의 상태 출력

--- src/main/java/RacingApplication.java
+++ src/main/java/RacingApplication.java
@@ -1,5 +1,28 @@
+import controller.CarController;
+import model.Car;
+import view.InputView;
+import view.ResultView;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class RacingApplication {
     public static void main(String[] args) {
+        CarController carController = new CarController();
+        InputView iv = new InputView();
+        List<Car> cars = new ArrayList<>();
 
+        long carNum = iv.inputTotalCarNum();
+        int trial = iv.inputTotalTrial();
+
+        carController.registerCars(cars, carNum);
+
+        for (int i = 0; i < trial; i++) {
+            carController.moveCar(cars);
+        }
+
+        for (Car car : cars) {
+            ResultView.printAllCarsState(car.moveCount());
+        }
     }
 }
