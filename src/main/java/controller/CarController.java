33f1d83af3e54327240382ed7528956e59f328a0
user <gusdn85554@naver.com>
2022-03-01 23:02:26 +0900

    feat : 자동차 등록 기능
    입력된 갯수에 따라 자동차를 등록

--- src/main/java/controller/CarController.java
+++ src/main/java/controller/CarController.java
@@ -1,4 +1,14 @@
 package controller;
 
+import model.Car;
+
+import java.util.List;
+
 public class CarController {
+
+    public void registerCars(List<Car> cars, long len) {
+        for (int i = 0; i < len; i++) {
+            cars.add(new Car(i));
+        }
+    }
 }
