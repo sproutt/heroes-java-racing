1d2ca78a57760a88ff6823da3dd8c6282e67bd74
user <gusdn85554@naver.com>
2022-03-01 22:52:39 +0900

    feat : ResultView 생성
    자동차가 이동한 길이에 따라 "-" 출력

--- src/main/java/view/ResultView.java
+++ src/main/java/view/ResultView.java
@@ -1,5 +1,15 @@
 package view;
 
+import model.Car;
+
+import java.util.List;
+
 public class ResultView {
 
+    public static void printAllCarsState(int len) {
+        for (int i = 0; i < len; i++) {
+            System.out.print("-");
+        }
+        System.out.println();
+    }
 }
