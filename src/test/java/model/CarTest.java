8187ea1fe844af951f935a8c5edd84ce43c79129
user <gusdn85554@naver.com>
2022-03-01 22:51:11 +0900

    test : Car 도메인 test
    move시 길이 변화 확인 및 식별자 확인

--- src/test/java/model/CarTest.java
+++ src/test/java/model/CarTest.java
@@ -1,39 +1,45 @@
 package model;
 
-import static org.junit.Assert.*;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
-import org.junit.Before;
-import org.junit.Test;
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
 
-public class CarTest {
+class CarTest {
 
-    private Car tesla;
-    private Car kia;
-    private Car hyundai;
+    @Test
+    @DisplayName("차 번호가 식별자이다")
+    public void identification() throws Exception{
+        //given
+        Car car1 = new Car(1L);
+        Car car2 = new Car(2L);
 
-    @Before
-    public void setup(){
-        tesla = new Car("tesla");
-        kia = new Car("kia");
-        hyundai = new Car("hyundai");
+        //when
+        long carNum1 = car1.getCarNum();
+        long carNum2 = car2.getCarNum();
+
+        //then
+        assertThat(carNum1).isEqualTo(1L);
+        assertThat(carNum2).isEqualTo(2L);
     }
 
     @Test
-    public void getNameTest() {
-        assertEquals("tesla", tesla.getName());
-        assertEquals("kia", kia.getName());
-        assertEquals("hyundai", hyundai.getName());
-    }
+    @DisplayName("차를 한 번 움직이면 length가 1 늘어나야 한다")
+    public void addLength() throws Exception{
+        //given
+        Car car1 = new Car(1L);
+        Car car2 = new Car(2L);
 
-    @Test
-    public void getPositionAndMoveTest(){
-        tesla.move(); tesla.move(); tesla.move();
-        assertEquals(3, tesla.getPosition());
+        //when
+        car1.moveForward();
+        car1.moveForward();
+        car2.moveForward();
 
-        kia.move(); kia.move();
-        assertEquals(2, kia.getPosition());
-
-        hyundai.move();
-        assertEquals(1, hyundai.getPosition());
+        //then
+        assertThat(car1.moveCount()).isEqualTo(2);
+        assertThat(car2.moveCount()).isEqualTo(1);
     }
 }
