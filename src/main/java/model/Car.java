78cd0dbb43cfa517009b098234044665306e2b32
user <gusdn85554@naver.com>
2022-03-01 22:50:29 +0900

    feat : Car 도메인 생성

--- src/main/java/model/Car.java
+++ src/main/java/model/Car.java
@@ -1,4 +1,24 @@
 package model;
 
 public class Car{
+
+    long number;
+    int length;
+
+    public Car(long number) {
+        this.number = number;
+        length = 0;
+    }
+
+    public void moveForward() {
+        length++;
+    }
+
+    public int moveCount() {
+        return length;
+    }
+
+    public long getCarNum() {
+        return number;
+    }
 }
