1f78f7a812ecf7133d09cc19007e106b7052c036
user <gusdn85554@naver.com>
2022-03-01 22:52:10 +0900

    feat : InputView 생성

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,5 +1,22 @@
 package view;
 
+import java.util.Scanner;
+
 public class InputView{
 
+    Scanner sc;
+
+    public InputView() {
+        sc = new Scanner(System.in);
+    }
+
+    public long inputTotalCarNum(){
+        System.out.println("자동차 대수는 몇 대 인가요?");
+        return sc.nextLong();
+    }
+
+    public int inputTotalTrial(){
+        System.out.println("시도할 횟수는 몇 회 인가요?");
+        return sc.nextInt();
+    }
 }
