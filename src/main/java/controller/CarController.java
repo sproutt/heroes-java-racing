0561026ba676df4a2f6f505f8cf830aa2e973ba3
user <gusdn85554@naver.com>
2022-03-01 23:08:30 +0900

    feat : 자동차 움직임 구현
    getRandomNumber : 랜덤 숫자 받아오기
    checkMoveOrStop : 움직일 수 있는지 판별
    moveCar : 자동차를 움직임

--- src/main/java/controller/CarController.java
+++ src/main/java/controller/CarController.java
@@ -1,14 +1,35 @@
 package controller;
 
+import function.RandomNumberMaker;
 import model.Car;
 
 import java.util.List;
 
 public class CarController {
 
+    private static final int NUMBER_RANGE = 10;
+    private static final int MOVE_CONDITION = 4;
+
     public void registerCars(List<Car> cars, long len) {
         for (int i = 0; i < len; i++) {
             cars.add(new Car(i));
         }
+    }
+
+    public void moveCar(List<Car> cars) {
+        for (Car car : cars) {
+            if(checkMoveOrStop(car)) car.moveForward();
+        }
+    }
+
+    private boolean checkMoveOrStop(Car car) {
+        if (getRandomNumber() >= MOVE_CONDITION) {
+            return true;
+        }
+        return false;
+    }
+
+    private int getRandomNumber() {
+        return RandomNumberMaker.getRandomNumber(NUMBER_RANGE);
     }
 }
--- src/main/java/function/RandomNumberMaker.java
+++ src/main/java/function/RandomNumberMaker.java
@@ -1,4 +1,10 @@
 package function;
 
+import java.util.Random;
+
 public class RandomNumberMaker {
+
+    public static int getRandomNumber(int range) {
+        return new Random().nextInt(range);
+    }
 }
