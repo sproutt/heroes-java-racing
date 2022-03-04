fe4856386a8e3b04176306b031ef2bd37114f7af
user <gusdn85554@naver.com>
2022-03-04 17:38:09 +0900

    feat : InputView 예외 처리
    0을 포함한 음수, 문자열이 입력될 경우 예외 처리 추가

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,5 +1,7 @@
 package view;
 
+import java.io.InputStream;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class InputView{
@@ -12,11 +14,30 @@ public class InputView{
 
     public long inputTotalCarNum(){
         System.out.println("자동차 대수는 몇 대 인가요?");
-        return sc.nextLong();
+        long carNum = 0L;
+        try {
+            carNum = Long.parseLong(sc.nextLine());
+            if (carNum <= 0)
+                throw new IllegalArgumentException();
+        } catch (IllegalArgumentException | InputMismatchException exception) {
+            System.out.println("자연수를 입력해주세요");
+            return inputTotalCarNum();
+        }
+        return carNum;
+
     }
 
     public int inputTotalTrial(){
         System.out.println("시도할 횟수는 몇 회 인가요?");
-        return sc.nextInt();
+        int trial = 0;
+        try {
+            trial = Integer.parseInt(sc.nextLine());
+            if (trial <= 0)
+                throw new IllegalArgumentException();
+        } catch (IllegalArgumentException | InputMismatchException exception) {
+            System.out.println("자연수를 입력해주세요");
+            return inputTotalTrial();
+        }
+        return trial;
     }
 }
