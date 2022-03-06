8d622d9a252233a3a300ea5fa6e3d9f65ba85841
sunghyuki <sunghyuk1609@gmail.com>
2022-03-06 15:50:27 +0900

    refactor : Car가 전진할 수 있는 기준인 4를 상수화

--- src/main/java/model/RandomMovingRule.java
+++ src/main/java/model/RandomMovingRule.java
@@ -2,9 +2,11 @@ package model;
 
 public class RandomMovingRule implements MovingRule {
 
+    private static final int CRITERIA_NUMBER = 4;
+
     @Override
     public boolean judge(int number) {
-        if(number >= 4) {
+        if(number >= CRITERIA_NUMBER) {
             return true;
         }
         return false;
